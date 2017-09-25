3c4eeb460c7f7e7f07960b0c218900811bd5d27a
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2017-09-26 00:09:23 +0200

    Edit plot scripts (and remove a data point) to make them actually work

--- results/plot_experiment.py
+++ results/plot_experiment.py
@@ -10,7 +10,7 @@ if len(argv) < 4:
 exper = pd.read_csv(argv[1], sep='\t', comment='#', float_precision="high", header=None, names=["m", "n", "k", "l", argv[2], "Goto", "error"])
 to_plot = exper[["m", argv[2], "Goto"]].copy()
 to_plot.set_index("m", inplace=True)
-ax = to_plot.plot(title=argv[3])
+ax = to_plot.plot(title=argv[3], xlim=(0, to_plot.index[-1]), ylim=(0, 50))
 ax.set_xlabel("N" if len(argv) == 4 else argv[4])
 ax.set_ylabel("GFlops/s")
 plt.show()
--- results/plot_multi_shape_experiment.py
+++ results/plot_multi_shape_experiment.py
@@ -9,7 +9,7 @@ if len(argv) < 3:
 
 narrow_exper = pd.read_csv(argv[1], sep='\t', comment='#', float_precision="high", header=None, names=["m", "n", "k", "l", argv[2], "Goto", "error"])
 narrow_exper["Narrowed Dim."] = narrow_exper[["m", "n", "k", "l"]].idxmin(axis=1)
-narrow_exper["N"] = narrow_exper.apply(lambda row: row["n"] if row["Narrowed Dim."] == "m" else row["m"], axis=1)
+narrow_exper["N"] = narrow_exper.apply(lambda row: row["k"] if row["Narrowed Dim."] == "m" else row["m"], axis=1)
 narrow_exper2 = narrow_exper.copy()
 narrow_exper2.set_index(["Narrowed Dim.", "N"], inplace=True)
 narrow_exper2.drop(["m", "n", "k", "l", "error"], axis=1, inplace=True)
@@ -20,7 +20,7 @@ for name, group in narrow_exper2.groupby(level="Narrowed Dim."):
     ax = fig.add_subplot(220 + i)
     group2 = group.copy()
     group2.index = group2.index.droplevel()
-    group2.plot(ax=ax, title="Narrow {}".format(name))
+    group2.plot(ax=ax, title="Narrow {}".format(name), xlim=(0, group2.index[-1]), ylim=(0, 50))
     ax.set_xlabel("Large dimensions")
     ax.set_ylabel("GFlops/s")
     i = i + 1
